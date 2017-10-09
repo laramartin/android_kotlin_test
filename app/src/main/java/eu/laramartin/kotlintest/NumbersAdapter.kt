6d8edb5d67aaf78b74c6928b1d027f3ae147ee10
Lara Martin <lara.martin.carretero@gmail.com>
2017-10-09 15:49:04 +0200

    add list row layout and adapter default template

---
+++ app/src/main/java/eu/laramartin/kotlintest/NumbersAdapter.kt
@@ -0,0 +1,25 @@
+package eu.laramartin.kotlintest
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class NumbersAdapter : RecyclerView.Adapter<NumbersAdapter.NumbersViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): NumbersViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: NumbersViewHolder?, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class NumbersViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+}
