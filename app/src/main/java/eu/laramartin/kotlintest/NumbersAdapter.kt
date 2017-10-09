5789fe9c6c5fb259c825fd57835848dd2347fa70
Lara Martin <lara.martin.carretero@gmail.com>
2017-10-09 17:13:03 +0200

    add kotlin android ext to import ids, populate list item

--- app/src/main/java/eu/laramartin/kotlintest/NumbersAdapter.kt
+++ app/src/main/java/eu/laramartin/kotlintest/NumbersAdapter.kt
@@ -1,25 +1,28 @@
 package eu.laramartin.kotlintest
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.list_row.view.*
 
-class NumbersAdapter : RecyclerView.Adapter<NumbersAdapter.NumbersViewHolder>() {
+class NumbersAdapter(private val numbers: List<Int>) : RecyclerView.Adapter<NumbersAdapter.NumbersViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): NumbersViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) : NumbersViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.list_row, parent, false)
+        return NumbersViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: NumbersViewHolder?, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: NumbersViewHolder, position: Int) {
+        holder.bind(numbers[position])
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemCount(): Int = numbers.size
 
-    class NumbersViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+    class NumbersViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(number: Int) {
+            itemView.number_text_view.setText(number)
+        }
     }
 
 }
